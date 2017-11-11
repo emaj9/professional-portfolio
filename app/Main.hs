ec1a777d23667c739153b1cf67c0fd6ae92e9abf
Eric Mayhew <ejmayhew@gmail.com>
2017-11-10 22:24:46 -0500

    Implement basic hakyll compiler
    * Compiled and route all pages to html
    * Compiled css by copying

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Hakyll
+import System.FilePath
+import Text.Pandoc
+import Data.Monoid ( (<>) )
+
+--hakyll :: monad -> IO ()
 
 main :: IO ()
-main = someFunc
+main = hakyll $ do
+  match "pages/*" $ do
+    route $ customRoute myRoute
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
+  match "templates/*" $ compile templateCompiler
+  match "css/style.css" $ do
+    route idRoute
+    compile copyFileCompiler
+
+myRoute :: (Identifier -> FilePath)
+myRoute id
+  | id == "pages/home.html" = "index.html"
+  | otherwise = dropExtension (toFilePath id) ++ ".html"
